8754d9e6d04a3fe186f034a89b01c89fd78d477f
Aldana <aldanamalgeri@hotmail.com>
2023-02-02 15:54:34 -0300

    proyecto listo

---
+++ scrip.js
@@ -0,0 +1,24 @@
+function myFunction(){
+    document.getElementById("ex").innerHTML = ""; 
+    document.getElementById("edu").innerHTML = ""; 
+   
+    document.getElementById("sobre").innerHTML = "<h3>Sobre mi</h3> <br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam saepe corporis distinctio repellat veritatis, facilis iusto nihil cum quia. Quia ex neque earum necessitatibus sunt nemo molestiae ipsum at doloribus! </p>"; 
+}
+
+function myFunctione(){
+    document.getElementById("sobre").innerHTML = "";
+    document.getElementById("ex").innerHTML = ""; 
+   //let pr =document.getElementById("prueba").innerHTML ;
+    //document.getElementById("edu");
+    document.getElementById("edu").innerHTML = " <h3>Educación</h3> <br><ul> <li>E.E.S.T.N°1</li>Título: Maestro Mayor de Obras  <br> Año de egreso: 2011 <br> <br><li>ISFD N°160</li> Título: Profesora de matemática  <br>  Año de egreso: 2016 <br> <br> <li>ISFT N°29</li>Título: Téctica superior en desarrollo de software  <br>En proceso <br> </ul>" ;   
+   
+    
+}
+
+function myFunctionex(){
+    document.getElementById("sobre").innerHTML = ""; 
+    document.getElementById("edu").innerHTML = ""; 
+    document.getElementById("ex").innerHTML = "<h3>Experiencia</h3> <br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam saepe corporis distinctio repellat veritatis, facilis iusto nihil cum quia. Quia ex neque earum necessitatibus sunt nemo molestiae ipsum at doloribus! </p>";  
+}
+
+
